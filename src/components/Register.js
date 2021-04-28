d913c3bf5da9cf9b2861a94c1fe2a21cf0f7e396
Faisal Feroze <faisal.feroze.1995@gmail.com>
2021-04-28 16:55:31 +0600

    registration api integrated

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import { useState } from 'react'
+
 
 const Register = () => {
+    const [name,setName] = useState('');
+    const [password,setPassword] = useState('');
+    const [email,setEmail] = useState('');
+
+    const signup = async ()=>{
+
+        let item = {name,email,password};
+        console.log('before hitting api',item);
+        const res = await fetch('http://127.0.0.1:8000/api/register',{
+            method:'POST',
+            body: JSON.stringify(item),
+            headers: 
+                {
+                    'Content-Type': "application/json",
+                    "Accept": "application/json"
+                }
+        })
+        const result = await res.json();
+        console.log(result);
+    }
+
     return (
-        <div>
+        <div className="col-sm-6 offset-sm-3">
             <h1>register</h1>
+            <input type="text" value={name} onChange={(e)=>setName(e.target.value)} className="form-control" placeholder="name"/>
+            <input type="email" value={email} onChange={(e=>setEmail(e.target.value))} className="form-control" placeholder="email"/>
+            <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} className="form-control" placeholder="password"/>
+            <button onClick={signup} className="btn btn-primary">Sign Up</button>
         </div>
     )
 }
